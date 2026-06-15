5f137f57db281b29188f9ab96274f4d13cf4807b
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-15 17:48:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,33 +13,51 @@ app.get("/", (req, res) => {
 let pendingRequests = {};
 
 io.on("connection", (socket) => {
-  console.log("User connected");
+  console.log("User connected:", socket.id);
 
+  // Join room directly (owner or accepted user)
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`Joined room: ${room}`);
+  });
+
+  // Send join request
   socket.on("join-request", (room) => {
     pendingRequests[room] = socket.id;
+
+    // send request to room (owner side)
     io.to(room).emit("new-request", room);
   });
 
+  // Accept request
   socket.on("accept-request", (room) => {
     const requester = pendingRequests[room];
 
     if (requester) {
       io.sockets.sockets.get(requester)?.join(room);
       io.to(requester).emit("request-accepted", room);
+
       delete pendingRequests[room];
     }
   });
 
-  socket.on("join-room", (room) => {
-    socket.join(room);
+  // ❌ Reject request
+  socket.on("reject-request", (room) => {
+    const requester = pendingRequests[room];
+
+    if (requester) {
+      io.to(requester).emit("request-rejected", room);
+      delete pendingRequests[room];
+    }
   });
 
+  // Message send
   socket.on("send-message", (data) => {
     io.to(data.room).emit("receive-message", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("User disconnected:", socket.id);
   });
 });
 
